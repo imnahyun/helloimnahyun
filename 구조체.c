0a9643b547391216dad9f302ceca8b8b1ca57c29
imnahyun <127062648+imnahyun@users.noreply.github.com>
2023-03-09 15:50:58 +0900

    Add files via upload

---
+++ 구조체.c
@@ -0,0 +1,18 @@
+struct ranking {
+
+	char username[1024];
+	int trycount;
+	int time;
+}typedef ranking;
+
+
+int main() {
+	int count = 10;
+	int trycount = count - trycount
+
+		struct player[10] = { username, trycount }  //gpt가 짜준코드.
+
+	printf("너의 이름을 입력하세요.");
+	scanf("%s", player1.username);
+
+	printf("유저의 이름:%s, 시도 횟수 %d, 걸린 시간 %d\n", ranking.username, ranking.trycount, rank.time)
